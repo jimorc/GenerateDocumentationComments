d3940d275044be32feee88fb7ad8f7b656330ee1
Jim <jimorcheson@yahoo.ca>
2017-09-18 06:50:48 -0400

    Added tests for summary documentation.

---
+++ GenDocCommentsTests/GenDocClassDeclarationTests.cs
@@ -0,0 +1,193 @@
+﻿using GenerateDocumentationComments;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace GenerateDocumentationCommentsTests
+{
+    public class GenDocClassDeclarationTests
+    {
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToIndented4SpacesPublicClassDeclaration()
+        {
+            var classDecl =
+                @"    public class Class1
+    {
+    }";
+            var expected =
+                @"    /// <summary>
+    /// 
+    /// </summary>
+    public class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new GenerateDocumentationComments.DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToIndented8SpacesPublicClassDeclaration()
+        {
+            var classDecl =
+                @"        public class Class1
+        {
+        }";
+            var expected =
+                @"        /// <summary>
+        /// 
+        /// </summary>
+        public class Class1
+        {
+        }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToIndentedNoSpacesPublicClassDeclaration()
+        {
+            var classDecl =
+                @"public class Class1
+{
+}";
+            var expected =
+                @"/// <summary>
+/// 
+/// </summary>
+public class Class1
+{
+}";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToIndented4SpacesProtectedClassDeclaration()
+        {
+            var classDecl =
+@"    protected class Class1
+    {
+    }";
+            var expected =
+@"    /// <summary>
+    /// 
+    /// </summary>
+    protected class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToInternalClassDeclaration()
+        {
+            var classDecl =
+@"    internal class Class1
+    {
+    }";
+            var expected =
+@"    /// <summary>
+    /// 
+    /// </summary>
+    internal class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldNotAddSummaryDocCommentsToPrivateClassDeclaration()
+        {
+            var classDecl =
+@"    private class Class1
+    {
+    }";
+            var expected =
+@"    private class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldNotAddSummaryDocCommentsToNoAccessClassDeclaration()
+        {
+            var classDecl =
+@"    class Class1
+    {
+    }";
+            var expected =
+    @"    class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldNotAddSummaryDocCommentsClassDeclarationWithSummaryComments()
+        {
+            var classDecl =
+@"    /// <summary>
+    /// A summary description
+    /// </summary>
+    public class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(classDecl, result.ToFullString());
+        }
+
+        [Fact]
+        public void ShouldNotAddSummaryDocCommentsClassDeclarationWithMultilineSummaryComments()
+        {
+            var classDecl =
+@"    /** <summary>
+         A summary description
+         </summary> */
+        public class Class1
+        {
+        }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+
+            var result = rewriter.Visit(tree.GetRoot());
+
+            Assert.Equal(classDecl, result.ToFullString());
+        }
+    }
+}
--- GenerateDocumentationComments/DocumentCommentsRewriter.cs
+++ GenerateDocumentationComments/DocumentCommentsRewriter.cs
@@ -5,14 +5,12 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-[assembly: InternalsVisibleTo("GenDocCommentsTests,PublicKey=0024000004800000" +
-                              "9400000006020000002400005253413100040000010001" +
-                              "00a18f862e7ba9c69c88135f4ab1f6b1412cc49de0a3b1" +
-                              "fa75e5f76889d2b20ddc41d91a3194053bbb3ba38e706b" +
-                              "f9e18078d40310ad659f2133441906200c69caec32f223" +
-                              "084d539d5edb445d0ca1b4e6d699ed7a94944a79f14f4b" +
-                              "82f61dfd606ffb1148f185444073aadb0024bac3161009" +
-                              "629c705fdc89002d93ccf17bf0a6")]
+[assembly: InternalsVisibleTo("GenerateDocumentationCommentsTests,PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010019aa30c" +
+    "7a9e5935e9ac11a6b35377b9afd0e5bd720b0f54628fb5e143d90f7e05696ea49062f2a" +
+    "2d4d06cae1cd6a774a285648846e0789ef89b66818496b7b4ebb58f3637df7f3059bd0b" +
+    "8dca6ac53e9618c8272572506d6df0fb37850c6603007a65c85eae18c4887ca04ed8b29" +
+    "c62339dd60c6f0643da8287f4cbe0c01279e")]
 
 namespace GenerateDocumentationComments
 {
